21b723ad573b07df61d740d9cbe4401bdce4f73b
Eric Tyler <83565207+tylereric24@users.noreply.github.com>
2023-07-16 15:35:29 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -340,7 +340,7 @@ export const managers = [
        "tookOver": 2021, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "San Diego", // (optional)
        "bio": "Tarnished former member of league best known for spending 20 real dollars to purchase Odell from Berto only to still lose",
-       "photo": null, // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/adrian.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": null, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": null, // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -363,7 +363,7 @@ export const managers = [
        "tookOver": 2021, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "San Diego", // (optional)
        "bio": "Former temporary member of league who took over Bertos roster during the Odell scandal of 2021. Unfortunate league champion of 2021. Bailed on us last minute before 2022 draft",
-       "photo": null, // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/ryan.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": null, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "sf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
