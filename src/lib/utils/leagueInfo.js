ad5e5c8be193fab37281709f470a907b5963503e
Eric Tyler <83565207+tylereric24@users.noreply.github.com>
2023-07-16 15:24:23 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -386,7 +386,7 @@ export const managers = [
        "tookOver": 2020, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "San Diego", // (optional)
        "bio": "Former league member of first year of the league. Had an unfortunately bad season and never returned. It is believed that he is now living in a cave in the Himalayas surviving on only buttermilk ranch dressing.",
-       "photo": "/managers/Ranchtan.jpg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/Ranchtan.JPG", // square ratio recommended (no larger than 500x500)
        "fantasyStart": null, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "den", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
